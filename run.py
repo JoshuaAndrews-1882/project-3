5d6d39179fd3156c638e9d09f337f413b4abee71
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-25 16:34:32 +0000

    Add quest 2

--- run.py
+++ run.py
@@ -4,16 +4,26 @@
 def pre():
     print()
     print("King Adelbern calls for all heroes to help fight Charr")
-    quest1 = input("do you join him? Y/N")
+    quest1 = input("do you join him? (Y/N)  ")
     if quest1 == "N":
         print("You die")
     elif quest1 == "Y":
+        print("You help fight the Charr")
         post()
 
+def post():
+    print()
+    print("You search around and find the King")
+    quest2 = input("Do stay with the King? (Y/N)  ")
+    if quest2 == "N":
+        print("You leave with the prince")
+        shiverpeaks()
+    elif quest2 == "Y":
+        print("You stay and retire")
 
 print("         Guild Wars          ")
 print(" Hello & welcome to Ascalon  ")
-start_game = input("Do you wish to be a Hero? (Y/N)")
+start_game = input("Do you wish to be a Hero? (Y/N)  ")
 
 if start_game == "Y":
     pre()
