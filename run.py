ea69fa46439746b61aa16359baf3cad9d3b07ed7
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-25 15:02:19 +0000

    Add initial start game screen and question

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
+print("         Guild Wars          ")
+print(" Hello & welcome to Ascalon  ")
+start_game = input("Do you wish to be a Hero? (Y/N)")
