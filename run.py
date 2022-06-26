7eb776dca578bed503704c4f4d4cd920f831f5e7
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-26 14:55:09 +0000

    Add while true code for when user doesn't put in the correct response, asks the question again

--- run.py
+++ run.py
@@ -1,40 +1,62 @@
 #intro
 def intro():
-    start_game = input("Do you wish to be a Hero? (Y/N)\n")
-    if start_game.upper().strip() == "Y":
-        pre_ascalon()
-    elif start_game.upper().strip() == "N":
-        print()
-        print("Goodbye")
+    while True:
+        start_game = input("Do you wish to be a Hero? (Y/N)\n")
+        if start_game.upper().strip() == "Y":
+            pre_ascalon()
+        elif start_game.upper().strip() == "N":
+            print()
+            print("Goodbye")
+        else:
+            print()
+            f = open("settings/yesno.text", "r")
+            print(f.read())
+            print()
 
+###PROPHECIES###
 #ascalon
 def pre_ascalon():
-    print()
-    f = open("prophecies/a-pre-ascalon/pre.text", "r")
-    print(f.read())
-    quest1 = input("Do you accept? (Y/N)\n")
-    if quest1.upper().strip() == "N":
+    while True:
         print()
-        print("The Charr eat you for breakfast")
-        intro()
-    elif quest1.upper().strip() == "Y":
-        print()
-        f = open("prophecies/a-pre-ascalon/pre-y", "r")
+        f = open("prophecies/a-pre-ascalon/pre.text", "r")
         print(f.read())
-        post_ascalon()
+        quest1 = input("Do you accept? (Y/N)\n")
+        if quest1.upper().strip() == "N":
+            print()
+            print("The Charr eat you for breakfast")
+            intro()
+        elif quest1.upper().strip() == "Y":
+            print()
+            f = open("prophecies/a-pre-ascalon/pre-y", "r")
+            print(f.read())
+            post_ascalon()
+        else:
+            print()
+            f = open("settings/yesno.text", "r")
+            print(f.read())
+            print()
 
 def post_ascalon():
-    print()
-    print("You search around and find the King")
-    quest2 = input("Do stay with the King? (Y/N)\n")
-    if quest2.upper().strip() == "N":
+    while True:
         print()
-        print("You leave with the prince")
-        shiverpeaks()
-    elif quest2.upper().strip() == "Y":
-        print()
-        print("You stay and retire \n")
-        intro()
+        print("You search around and find the King")
+        quest2 = input("Do stay with the King? (Y/N)\n")
+        if quest2.upper().strip() == "N":
+            print()
+            print("You leave with the prince")
+            shiverpeaks()
+        elif quest2.upper().strip() == "Y":
+            print()
+            print("You stay and retire \n")
+            intro()
+        else:
+            print()
+            f = open("settings/yesno.text", "r")
+            print(f.read())
+            print()
+
+#shiverpeaks
+
 
 #title
 print("         Guild Wars          ")
