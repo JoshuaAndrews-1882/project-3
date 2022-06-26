f4b9c94b4abb72b7b23a14c52989d27cb2317380
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-26 14:35:46 +0000

    Have user input always be converted to upper and strip any empty spaces

--- run.py
+++ run.py
@@ -1,38 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-def pre():
+#intro
+def intro():
+    start_game = input("Do you wish to be a Hero? (Y/N)\n")
+    if start_game.upper().strip() == "Y":
+        pre_ascalon()
+    elif start_game.upper().strip() == "N":
+        print()
+        print("Goodbye")
+
+#ascalon
+def pre_ascalon():
     print()
-    f = open("story/pre-ascalon/pre.text", "r")
+    f = open("prophecies/a-pre-ascalon/pre.text", "r")
     print(f.read())
     quest1 = input("Do you accept? (Y/N)\n")
-    if quest1 == "N":
+    if quest1.upper().strip() == "N":
         print()
-        print("You die")
-    elif quest1 == "Y":
+        print("The Charr eat you for breakfast")
+        intro()
+    elif quest1.upper().strip() == "Y":
         print()
-        f = open("story/pre-ascalon/pre-y", "r")
+        f = open("prophecies/a-pre-ascalon/pre-y", "r")
         print(f.read())
-        post()
+        post_ascalon()
 
-def post():
+def post_ascalon():
     print()
     print("You search around and find the King")
     quest2 = input("Do stay with the King? (Y/N)\n")
-    if quest2 == "N":
+    if quest2.upper().strip() == "N":
         print()
         print("You leave with the prince")
         shiverpeaks()
-    elif quest2 == "Y":
+    elif quest2.upper().strip() == "Y":
         print()
-        print("You stay and retire")
+        print("You stay and retire \n")
+        intro()
 
+#title
 print("         Guild Wars          ")
 print(" Hello & welcome to Ascalon  ")
-start_game = input("Do you wish to be a Hero? (Y/N)\n")
-
-if start_game == "Y":
-    pre()
-elif start_game == "N":
-    print()
-    print("Goodbye")
+intro()
