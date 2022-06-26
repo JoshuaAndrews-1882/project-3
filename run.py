9e2ced3fcee2da0d98601e791cb37ce226006961
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-26 15:57:42 +0000

    Add shiverpeaks section

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #intro
 def intro():
     while True:
+        print()
         start_game = input("Do you wish to be a Hero? (Y/N)\n")
         if start_game.upper().strip() == "Y":
             pre_ascalon()
@@ -13,21 +14,23 @@ def intro():
             print(f.read())
             print()
 
-###PROPHECIES###
+#PROPHECIES
+
 #ascalon
 def pre_ascalon():
     while True:
         print()
-        f = open("prophecies/a-pre-ascalon/pre.text", "r")
+        f = open("prophecies/pre-ascalon/pre.text", "r")
         print(f.read())
         quest1 = input("Do you accept? (Y/N)\n")
         if quest1.upper().strip() == "N":
             print()
-            print("The Charr eat you for breakfast")
+            f = open("prophecies/pre-ascalon/pre-n.text", "r")
+            print(f.read())
             intro()
         elif quest1.upper().strip() == "Y":
             print()
-            f = open("prophecies/a-pre-ascalon/pre-y", "r")
+            f = open("prophecies/pre-ascalon/pre-y.text", "r")
             print(f.read())
             post_ascalon()
         else:
@@ -39,23 +42,46 @@ def pre_ascalon():
 def post_ascalon():
     while True:
         print()
-        print("You search around and find the King")
+        f = open("prophecies/post-ascalon/post.text", "r")
+        print(f.read())
         quest2 = input("Do stay with the King? (Y/N)\n")
         if quest2.upper().strip() == "N":
             print()
-            print("You leave with the prince")
+            f = open("prophecies/post-ascalon/post-n.text", "r")
+            print(f.read())
             shiverpeaks()
         elif quest2.upper().strip() == "Y":
             print()
-            print("You stay and retire \n")
+            f = open("prophecies/post-ascalon/post-y.text", "r")
+            print(f.read())
             intro()
         else:
             print()
             f = open("settings/yesno.text", "r")
             print(f.read())
-            print()
 
 #shiverpeaks
+def shiverpeaks():
+    while True:
+        print()
+        f = open("prophecies/shiverpeaks/shiver.text", "r")
+        print(f.read())
+        quest3 = input("Do you help the Deldrimor Dwarves (Y/N)\n")
+        if quest3.upper().strip() == "N":
+            print()
+            f = open("prophecies/shiverpeaks/shiver-n.text", "r")
+            print(f.read())
+            intro()
+        elif quest3.upper().strip() == "Y":
+            print()
+            f = open("prophecies/shiverpeaks/shiver-y.text", "r")
+            print(f.read())
+            kryta()
+        else:
+            print()
+            f = open("settings/yesno.text", "r")
+            print(f.read())
+
 
 
 #title
